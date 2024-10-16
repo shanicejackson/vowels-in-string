d2eead236d4f1d6ab54316388d94897cd2cff3da
Shanice Jackson <shanicejack12@icloud.com>
2024-10-16 14:51:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+I will be needing to find a string function to count
+the number of vowels in a string
+
+I will also be needing a variable that keep all the vowels
+
+
+Coding tools: functions, Varables, For loop
+"""
+"""
+
+"""
+
+def count_vowels(s):
+    pass
